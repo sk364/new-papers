e90a428a0f378ca56bcc25abec19bcf451d3377b
Sachin Kukreja <sachin.kukreja@walmartlabs.com>
2021-06-15 23:51:11 +0530

    Integrate etl module; add route to setup the data

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from etl import setup, get_similar_papers
 
 
 app = Flask(__name__)
@@ -8,19 +9,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/setup')
+def app_setup():
+    try:
+        setup()
+        return "Success"
+    except:
+        return "Something went wrong!"
 
 
 @app.route("/search", methods=["POST"])
 def search():
-  data = [{
-    "id": 1,
-    "title": "title",
-    "abstract": "abs"
-  },
-  {
-    "id": 1,
-    "title": "title",
-    "abstract": "abs"
-  }]
-  return json.dumps(data)
+    req_json = request.json
+    abstract = req_json.get("abstract")
+
+    data = []
+    if abstract:
+        data = get_similar_papers(abstract)
+
+    return json.dumps(data)
